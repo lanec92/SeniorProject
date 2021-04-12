335b8e8f9abe9f3bc4775d91dd710a22bf9e2a3f
Lane <lanec92@gmail.com>
2021-04-12 05:10:08 -0500

    Cleaning Up

--- frmReservation/frmLogin.Designer.cs
+++ frmReservation/frmLogin.Designer.cs
@@ -108,7 +108,7 @@ namespace frmReservation
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "frmLogin";
-            this.Text = "frmLogin";
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
